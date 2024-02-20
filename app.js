4abcaaf2e4983a3844a49797115084ed899d23b1
tteemmaj <epro639@gmail.com>
2024-02-19 19:43:44 -0600

    Primera version

---
+++ app.js
@@ -0,0 +1,105 @@
+let usuarioTexto = document.querySelector('#texto-usuario');
+let encriptarBtn = document.querySelector('.encriptar-btn');
+let desencriptarBtn = document.querySelector('.desencriptar-btn');
+let copiarBoton = document.querySelector('.vopiar-boton');
+let botonActivo = false;
+let textoResultado = document.querySelector('#texto-resultado');
+let imgDerecha = document.querySelector('.img-derecha');
+let tituloDerecha = document.querySelector('.titulo-texto');
+let texto = "";
+
+
+function mostrarBoton() {
+    if (botonActivo = false) {
+        copiarBoton.style.display = "none"
+    } else {
+        copiarBoton.style.display = "inline-block"
+    }
+}
+
+function comprobar () {
+    if (usuarioTexto.value === "") {
+        alert("Tienes que escribir algo");
+    } else{
+        imgDerecha.style.display = "none";
+        tituloDerecha.style.display = "none";
+        let espacioResultado = document.getElementById('texto-resultado');
+        espacioResultado.style.height = "90%"
+        espacioResultado.style.textAlign = "left";
+        mostrarBoton();
+        botonActivo = true;
+    }
+} 
+
+/* encriptar */
+
+encriptarBtn.addEventListener('click', () => {
+    if (usuarioTexto.value === "") {
+        comprobar();
+    } else{
+        encritar();
+        comprobar();
+    }
+    texto = ""; 
+})
+
+function encritar () {
+    let encriptarTexto = usuarioTexto.value
+    let finalText = encriptarTexto.toLowerCase();
+    for (let i = 0; i < finalText.length; i++) {
+        let element = finalText[i];
+        switch (element) {
+            case "a":
+                element = "ai"
+                break;
+            case "e":
+                element = "enter"
+                break;
+            case "i":
+                element = "imes"
+                break;
+            case "o":
+                element = "ober"
+                break;
+            case "u":
+                element = "ufat"
+                break;
+            default:
+                break;
+        }
+        texto += element;
+    }
+    textoResultado.textContent = texto;
+}
+
+/* Desencriptar */
+
+function desencriptar() {
+    let textoEncriptado = usuarioTexto.value
+    let textoEncriptadoFinal = textoEncriptado.toLowerCase();
+    textoEncriptadoFinal = textoEncriptadoFinal.replace(/ai/g, "a");
+    textoEncriptadoFinal = textoEncriptadoFinal.replace(/enter/g, "e");
+    textoEncriptadoFinal = textoEncriptadoFinal.replace(/imes/g, "i");
+    textoEncriptadoFinal = textoEncriptadoFinal.replace(/ober/g, "o");
+    textoEncriptadoFinal = textoEncriptadoFinal.replace(/ufat/g, "u");
+    textoResultado.textContent = textoEncriptadoFinal;
+}
+
+desencriptarBtn.addEventListener('click', () => {
+    if (usuarioTexto.value === "") {
+        comprobar();
+    } else {
+        desencriptar();
+        comprobar();
+    }
+    texto = ""; 
+})
+
+
+/* Copiar texto encriptado */
+copiarBoton.addEventListener('click', () => {
+    let textoEncriptado = document.getElementById('texto-resultado');
+    textoEncriptado.focus();
+    document.execCommand('selectAll');
+    document.execCommand('copy');
+})
